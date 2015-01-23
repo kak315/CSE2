b587680049544927a0feb754365f909d9dffeb59
Kathryn Kundrod <kak315@lehigh.edu>
2015-01-23 18:55:20 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Kathryn Kundrod
+//hw01
+//Welcome Class Java Program
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+public static void main(String[] args) {
+
+//  print the statement
+//  input double backslash to print a single backslash
+    System.out.println("  -----------"    );
+    System.out.println("  | WELCOME |"    );
+    System.out.println("  -----------"    );
+    System.out.println("  ^  ^  ^  ^  ^  ^"  );
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\" );
+    System.out.println("<-K--A--K--3--1--5->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /" ); 
+    System.out.println("  v  v  v  v  v  v"  );
+    System.out.println("I was born on a Friday the 13th. Also, I studied abroad in Argentina last year.");
+    
+    }
+}
